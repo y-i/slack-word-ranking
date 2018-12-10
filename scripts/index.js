70ee77939ca39fb2e7ddbb1677327682f145d52b
y-i <y-i@users.noreply.github.com>
2018-12-10 15:56:56 +0900

    add ranking about 1 day and 1 week

--- scripts/index.js
+++ scripts/index.js
@@ -30,6 +30,7 @@ module.exports = (robot) => {
     const text = msg.match[1];
     try {
       const result = await mecab.parse(text);
+      /*
       let message = '';
       message += '名詞: ';
       message += result.filter(el => el[1] === '名詞').map(el => el[0]).join(', ');
@@ -53,6 +54,7 @@ module.exports = (robot) => {
       message += result.filter(el => el[1] === '連体詞').map(el => el[0]).join(',');
       message += '\n';
       msg.send(message);
+      */
       // insert into aggregation (word, type, count, day) values (?,名詞,1,?) where word = ? and type = 名詞 and day = ? ON DUPLICATE KEY UPDATE count = count + 1;
       result.filter(el => el[1] === '名詞').map(el => el[0]).forEach(el => {
         connection.query('insert into aggregation (word, type, count, day) values (?,"名詞",1,?) ON DUPLICATE KEY UPDATE count = count + 1;', [el, formatDate()], (error, results, fields) => {
@@ -70,7 +72,7 @@ module.exports = (robot) => {
         });
       });
       result.filter(el => el[1] === '副詞').map(el => el[0]).forEach(el => {
-        connection.query('insert into aggregation (word, type, count, day) values (?,"副詞,"1,?) ON DUPLICATE KEY UPDATE count = count + 1;', [el, formatDate()], (error, results, fields) => {
+        connection.query('insert into aggregation (word, type, count, day) values (?,"副詞",1,?) ON DUPLICATE KEY UPDATE count = count + 1;', [el, formatDate()], (error, results, fields) => {
           if (error) console.log(error);
         });
       });
@@ -95,10 +97,15 @@ module.exports = (robot) => {
   });
 
   robot.respond(/ranking/, (msg) => {
-    connection.query('select word, count, type from aggregation where day >= ? - interval 1 day order by count desc limit 10', [formatDate()], (error, results, fields) => {
+    connection.query('select word, type, sum(count) as count from aggregation where day >= ? - interval 1 day and char_length(word) >= 2 and word != "ranking" and word != "mecab" group by word, type order by count desc limit 10', [formatDate()], (error, results, fields) => {
       if (error) console.log(error);
       const res = results.map(row => `${row.count}: ${row.word} (${row.type})`);
-      msg.send(res.join('\n'));
+      msg.send('*昨日からのランキング*\n' + res.join('\n'));
+    });
+    connection.query('select word, type, sum(count) as count from aggregation where day >= ? - interval 7 day and char_length(word) >= 2 and word != "ranking" and word != "mecab" group by word, type order by count desc limit 10', [formatDate()], (error, results, fields) => {
+      if (error) console.log(error);
+      const res = results.map(row => `${row.count}: ${row.word} (${row.type})`);
+      msg.send('*一週間のランキング*\n' + res.join('\n'));
     });
   });
 };
